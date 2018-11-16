cb07a49eb1a79527509e9955a97632de565969ab
Kevin <kevin.coatsworth@digital.hmrc.gov.uk>
2018-11-16 12:31:33 +0000

    Refactored code and tests.

--- src/main/scala/BerlinClock.scala
+++ src/main/scala/BerlinClock.scala
@@ -1,74 +1,96 @@
+sealed trait LampStatus
+
+case object R extends LampStatus
+case object Y extends LampStatus
+case object O extends LampStatus
+
+case class BerlinClockTime(topHours: TopHours, bottomHours: BottomHours, topMinutes: TopMinutes, bottomMinutes: BottomMinutes, seconds: Seconds)
+case class TopHours(lamp1: LampStatus, lamp2: LampStatus, lamp3: LampStatus, lamp4: LampStatus)
+case class BottomHours(lamp1: LampStatus, lamp2: LampStatus, lamp3: LampStatus, lamp4: LampStatus)
+case class TopMinutes(lamp1: LampStatus, lamp2: LampStatus, lamp3: LampStatus, lamp4: LampStatus, lamp5: LampStatus, lamp6: LampStatus,
+                      lamp7: LampStatus, lamp8: LampStatus, lamp9: LampStatus, lamp10: LampStatus, lamp11: LampStatus)
+case class BottomMinutes(lamp1: LampStatus, lamp2: LampStatus, lamp3: LampStatus, lamp4: LampStatus)
+case class Seconds(lamp1: LampStatus)
+
 object BerlinClock {
 
-  def secondsLampStatus(numOfSeconds: Int): String = {
+  def berlinClockTimeConversion(hours: Int, minutes: Int, seconds: Int): BerlinClockTime = {
+
+    val fiveHours: TopHours = fiveHourlyLamps(hours)
+    val oneHours: BottomHours = oneHourlyLamps(hours % 5)
+    val fiveMinutes: TopMinutes = fiveMinuteLamps(minutes / 5)
+    val oneMinutes: BottomMinutes = oneMinuteLamps(minutes % 5)
+    val secondsStatus: Seconds = secondsLampStatus(seconds)
+
+    BerlinClockTime(fiveHours, oneHours, fiveMinutes, oneMinutes, secondsStatus)
+
+  }
+
+  private def secondsLampStatus(numOfSeconds: Int): Seconds = {
 
     numOfSeconds match {
 
-      case seconds if seconds % 2 == 0 => "Y"
-      case _ => "O"
+      case seconds if seconds % 2 == 0 => Seconds(O)
+      case _ => Seconds(Y)
 
     }
   }
 
-  def topHourLamps(numOfHours: Int): String = {
+  private def fiveHourlyLamps(hoursFives: Int): TopHours = {
 
-    numOfHours match {
+    hoursFives match {
 
-      case hours if hours > 19 => "RRRR"
-      case hours if hours > 14 => "RRRO"
-      case hours if hours > 9 => "RROO"
-      case hours if hours > 4 => "ROOO"
-      case _ => "OOOO"
-
-    }
-  }
-
-  def bottomHourLamps(numOfHours: Int): String = {
-
-    //val remainderHours = numOfHours % 5
-
-    numOfHours match {
-
-      case 1 => "ROOO"
-      case 2 => "RROO"
-      case 3 => "RRRO"
-      case 4 => "RRRR"
-      case _ => "OOOO"
+      case hours if hours > 19 => TopHours(R, R, R, R)
+      case hours if hours > 14 => TopHours(R, R, R, O)
+      case hours if hours > 9 => TopHours(R, R, O, O)
+      case hours if hours > 4 => TopHours(R, O, O, O)
+      case _ => TopHours(O, O, O, O)
 
     }
   }
 
-  def topMinuteLamps(numOfMinutes: Int): String = {
+  private def oneHourlyLamps(hoursOnes: Int): BottomHours = {
 
-    val numOfLamps = numOfMinutes / 5
+    hoursOnes match {
 
-    numOfLamps match {
-
-      case 1 => "YOOOOOOOOOO"
-      case 2 => "YYOOOOOOOOO"
-      case 3 => "YYROOOOOOOO"
-      case 4 => "YYRYOOOOOOO"
-      case 5 => "YYRYYOOOOOO"
-      case 6 => "YYRYYROOOOO"
-      case 7 => "YYRYYRYOOOO"
-      case 8 => "YYRYYRYYOOO"
-      case 9 => "YYRYYRYYROO"
-      case 10 => "YYRYYRYYRYO"
-      case 11 => "YYRYYRYYRYY"
-      case _ => "OOOOOOOOOOO"
+      case 1 => BottomHours(R, O, O, O)
+      case 2 => BottomHours(R, R, O, O)
+      case 3 => BottomHours(R, R, R, O)
+      case 4 => BottomHours(R, R, R, R)
+      case _ => BottomHours(O, O, O, O)
 
     }
   }
 
-  def bottomMinuteLamps(numOfMinutes: Int): String = {
+  private def fiveMinuteLamps(minutesFives: Int): TopMinutes = {
 
-    numOfMinutes match {
+    minutesFives match {
 
-      case 1 => "YOOO"
-      case 2 => "YYOO"
-      case 3 => "YYYO"
-      case 4 => "YYYY"
-      case _ => "OOOO"
+      case 1 => TopMinutes(Y, O, O, O, O, O, O, O, O, O, O)
+      case 2 => TopMinutes(Y, Y, O, O, O, O, O, O, O, O, O)
+      case 3 => TopMinutes(Y, Y, R, O, O, O, O, O, O, O, O)
+      case 4 => TopMinutes(Y, Y, R, Y, O, O, O, O, O, O, O)
+      case 5 => TopMinutes(Y, Y, R, Y, Y, O, O, O, O, O, O)
+      case 6 => TopMinutes(Y, Y, R, Y, Y, R, O, O, O, O, O)
+      case 7 => TopMinutes(Y, Y, R, Y, Y, R, Y, O, O, O, O)
+      case 8 => TopMinutes(Y, Y, R, Y, Y, R, Y, Y, O, O, O)
+      case 9 => TopMinutes(Y, Y, R, Y, Y, R, Y, Y, R, O, O)
+      case 10 => TopMinutes(Y, Y, R, Y, Y, R, Y, Y, R, Y, O)
+      case 11 => TopMinutes(Y, Y, R, Y, Y, R, Y, Y, R, Y, Y)
+      case _ => TopMinutes(O, O, O, O, O, O, O, O, O, O, O)
+
+    }
+  }
+
+  private def oneMinuteLamps(minutesOnes: Int): BottomMinutes = {
+
+    minutesOnes match {
+
+      case 1 => BottomMinutes(Y, O, O, O)
+      case 2 => BottomMinutes(Y, Y, O, O)
+      case 3 => BottomMinutes(Y, Y, Y, O)
+      case 4 => BottomMinutes(Y, Y, Y, Y)
+      case _ => BottomMinutes(O, O, O, O)
 
     }
   }
--- src/test/scala/BerlinClockSpec.scala
+++ src/test/scala/BerlinClockSpec.scala
@@ -1,159 +1,197 @@
 import BerlinClock._
 import org.scalatest.{MustMatchers, WordSpec}
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 
 class BerlinClockSpec extends WordSpec with MustMatchers {
 
-  "Seconds Lamp Status" must {
+//  "Seconds Lamp Status" must {
+//
+//    "return false when given 0" in {
+//
+//      secondsLampStatus(0) mustEqual "O"
+//    }
+//
+//    "return true when given 1" in {
+//
+//      secondsLampStatus(1) mustEqual "Y"
+//    }
+//
+//    "return false when given 10" in {
+//
+//      secondsLampStatus(10) mustEqual "O"
+//    }
+//
+//    "return true when given 39" in {
+//
+//      secondsLampStatus(39) mustEqual "Y"
+//    }
+//
+//    "return false when given 62" in {
+//
+//      secondsLampStatus(62) mustEqual "O"
+//    }
+//  }
+//
+//  "Top Hour Lamps" must {
+//
+//    "return 'OOOO' when given 0" in {
+//
+//      topHourLamps(0) mustEqual "OOOO"
+//
+//    }
+//
+//    "return 'ROOO' when given 5" in {
+//
+//      topHourLamps(5) mustEqual "ROOO"
+//
+//    }
+//
+//    "return 'RROO' when given 10" in {
+//
+//      topHourLamps(10) mustEqual "RROO"
+//
+//    }
+//
+//    "return 'RRRO' when given 17" in {
+//
+//      topHourLamps(17) mustEqual "RRRO"
+//
+//    }
+//
+//    "return 'RRRR' when given 22" in {
+//
+//      topHourLamps(22) mustEqual "RRRR"
+//
+//    }
+//  }
+//
+//  "Bottom Hour Lamps" must {
+//
+//    "return 'OOOO' when given 0" in {
+//
+//      bottomHourLamps(0) mustEqual "OOOO"
+//
+//    }
+//
+//    "return 'ROOO' when given 1" in {
+//
+//      bottomHourLamps(1) mustEqual "ROOO"
+//
+//    }
+//
+//    "return 'RROO' when given 2" in {
+//
+//      bottomHourLamps(2) mustEqual "RROO"
+//
+//    }
+//
+//    "return 'RRRO' when given 3" in {
+//
+//      bottomHourLamps(3) mustEqual "RRRO"
+//
+//    }
+//
+//    "return 'RRRR' when given 4" in {
+//
+//      bottomHourLamps(4) mustEqual "RRRR"
+//
+//    }
+//  }
+//
+//  "Top Minute Lamps" must {
+//
+//    "return 'OOOOOOOOOOO' when given 0" in {
+//
+//      topMinuteLamps(0) mustEqual "OOOOOOOOOOO"
+//
+//    }
+//
+//    "return 'YOOOOOOOOOO' when given 5" in {
+//
+//      topMinuteLamps(5) mustEqual "YOOOOOOOOOO"
+//
+//    }
+//
+//    "return 'YYROOOOOOOO' when given 16" in {
+//
+//      topMinuteLamps(16) mustEqual "YYROOOOOOOO"
+//
+//    }
+//
+//    "return 'YYRYYRYYRYY' when given 57" in {
+//
+//      topMinuteLamps(57) mustEqual "YYRYYRYYRYY"
+//
+//    }
+//  }
+//
+//  "Bottom Minute Lamps" must {
+//
+//    "return 'OOOO' when given 0" in {
+//
+//      bottomMinuteLamps(0) mustEqual "OOOO"
+//
+//    }
+//
+//    "return 'YOOO' when given 1" in {
+//
+//      bottomMinuteLamps(1) mustEqual "YOOO"
+//
+//    }
+//
+//    "return 'YYOO' when given 2" in {
+//
+//      bottomMinuteLamps(2) mustEqual "YYOO"
+//
+//    }
+//
+//    "return 'YYYO' when given 3" in {
+//
+//      bottomMinuteLamps(3) mustEqual "YYYO"
+//
+//    }
+//
+//    "return 'YYYY' when given 4" in {
+//
+//      bottomMinuteLamps(4) mustEqual "YYYY"
+//
+//    }
+//  }
 
-    "return false when given 0" in {
+  "Berlin Clock Time Conversion" must {
 
-      secondsLampStatus(0) mustEqual "Y"
+    val date = Calendar.getInstance.getTime
+    val currentHour = new SimpleDateFormat("hh").format(date).toInt
+    val currentMinute = new SimpleDateFormat("mm").format(date).toInt
+    val currentSecond = new SimpleDateFormat("ss").format(date).toInt
+
+    "return 'OOOO, OOOO, OOOOOOOOOOO, OOOO, O' when given '0, 0, 0'" in {
+
+      berlinClockTimeConversion(0, 0, 0) mustEqual BerlinClockTime(TopHours(O, O, O, O), BottomHours(O, O, O, O), TopMinutes(O, O, O, O, O, O, O, O, O, O, O),
+        BottomMinutes(O, O, O, O), Seconds(O))
+
     }
 
-    "return true when given 1" in {
+    "return 'RROO, RRRO, YYROOOOOOOO, YOOO, Y' when given '13, 16, 3'" in {
 
-      secondsLampStatus(1) mustEqual "O"
-    }
-
-    "return false when given 10" in {
-
-      secondsLampStatus(10) mustEqual "Y"
-    }
-
-    "return true when given 39" in {
-
-      secondsLampStatus(39) mustEqual "O"
-    }
-
-    "return false when given 62" in {
-
-      secondsLampStatus(62) mustEqual "Y"
-    }
-  }
-
-  "Top Hour Lamps" must {
-
-    "return 'OOOO' when given 0" in {
-
-      topHourLamps(0) mustEqual "OOOO"
+      berlinClockTimeConversion(13, 16, 3) mustEqual BerlinClockTime(TopHours(R, R, O, O), BottomHours(R, R, R, O), TopMinutes(Y, Y, R, O, O, O, O, O, O, O, O),
+        BottomMinutes(Y, O, O, O), Seconds(Y))
 
     }
 
-    "return 'ROOO' when given 5" in {
+    "return 'RRRR, RRRR, YYRYYRYYRYY, YYYY, Y' when given '23, 59, 59'" in {
 
-      topHourLamps(5) mustEqual "ROOO"
-
-    }
-
-    "return 'RROO' when given 10" in {
-
-      topHourLamps(10) mustEqual "RROO"
+      berlinClockTimeConversion(23, 59, 59) mustEqual BerlinClockTime(TopHours(R, R, R, R), BottomHours(R, R, R, O), TopMinutes(Y, Y, R, Y, Y, R, Y, Y, R, Y, Y),
+        BottomMinutes(Y, Y, Y, Y), Seconds(Y))
 
     }
 
-    "return 'RRRO' when given 17" in {
+    "return correct response to current time" in {
 
-      topHourLamps(17) mustEqual "RRRO"
-
-    }
-
-    "return 'RRRR' when given 22" in {
-
-      topHourLamps(22) mustEqual "RRRR"
-
-    }
-  }
-
-  "Bottom Hour Lamps" must {
-
-    "return 'OOOO' when given 0" in {
-
-      bottomHourLamps(0) mustEqual "OOOO"
-
-    }
-
-    "return 'ROOO' when given 1" in {
-
-      bottomHourLamps(1) mustEqual "ROOO"
-
-    }
-
-    "return 'RROO' when given 2" in {
-
-      bottomHourLamps(2) mustEqual "RROO"
-
-    }
-
-    "return 'RRRO' when given 3" in {
-
-      bottomHourLamps(3) mustEqual "RRRO"
-
-    }
-
-    "return 'RRRR' when given 4" in {
-
-      bottomHourLamps(4) mustEqual "RRRR"
-
-    }
-  }
-
-  "Top Minute Lamps" must {
-
-    "return 'OOOOOOOOOOO' when given 0" in {
-
-      topMinuteLamps(0) mustEqual "OOOOOOOOOOO"
-
-    }
-
-    "return 'YOOOOOOOOOO' when given 5" in {
-
-      topMinuteLamps(5) mustEqual "YOOOOOOOOOO"
-
-    }
-
-    "return 'YYROOOOOOOO' when given 16" in {
-
-      topMinuteLamps(16) mustEqual "YYROOOOOOOO"
-
-    }
-
-    "return 'YYRYYRYYRYY' when given 57" in {
-
-      topMinuteLamps(57) mustEqual "YYRYYRYYRYY"
-
-    }
-  }
-
-  "Bottom Minute Lamps" must {
-
-    "return 'OOOO' when given 0" in {
-
-      bottomMinuteLamps(0) mustEqual "OOOO"
-
-    }
-
-    "return 'YOOO' when given 1" in {
-
-      bottomMinuteLamps(1) mustEqual "YOOO"
-
-    }
-
-    "return 'YYOO' when given 2" in {
-
-      bottomMinuteLamps(2) mustEqual "YYOO"
-
-    }
-
-    "return 'YYYO' when given 3" in {
-
-      bottomMinuteLamps(3) mustEqual "YYYO"
-
-    }
-
-    "return 'YYYY' when given 4" in {
-
-      bottomMinuteLamps(4) mustEqual "YYYY"
+      berlinClockTimeConversion(currentHour, currentMinute, currentSecond) mustEqual BerlinClockTime(TopHours(R, R, R, R), BottomHours(R, R, R, O), TopMinutes(Y, Y, R, Y, Y, R, Y, Y, R, Y, Y),
+        BottomMinutes(Y, Y, Y, Y), Seconds(Y))
 
     }
   }
